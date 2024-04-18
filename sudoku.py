b581019176fe53e68a8a96e7d8555394d9293e5e
Charlie Van Uffelen <charlie.vanuffelen@student.uclouvain.be>
2024-04-18 11:20:23 +0200

    Finis le projet Lol

--- sudoku.py
+++ sudoku.py
@@ -2,36 +2,37 @@ import random
 import time
 import math
 import sys
-
+from copy import deepcopy
  
 
 def objective_score(board):
+    # Return the score of conflicts in the board
     score = 0
     for i in range(9)  : 
         row = board[i]
-        col = [row[i] for row in board] 
-        block = get_block(board,i)
-        score += conflicts(row)
-        score += conflicts(col)
-        score += conflicts(block)
+        score += conflicts(row) #Check for row
+        
+    for i in range(9) : 
+        col = [rows[i] for rows in board] 
+        score += conflicts(col) #Check for col
     return score
-    
-def find_empty(board) : 
-    for i in range(len(board)) :
-        for j in range(len(board[0])) :
-            if board[i][j] == 0 : 
-                return (i,j)
-    return None
 
-def get_block(board,i) :
+def get_block(board,i,ignore=False,initials=None) :
+    # Return the ith block of the board as a list
     row = (i // 3) * 3
     col = (i % 3 )* 3
     arr = []
     for j in range(3) :
         for k in range(3) :
-            arr.append(board[row+j][col+k])
+            if ignore : 
+                if (row+j,col+k) not in initials : 
+                    arr.append((row+j,col+k))
+            else : 
+                arr.append(board[row+j][col+k])
     return arr
+
 def conflicts(board)  : 
+    # Return the score in a single array
     list =  []
     score = 0 
     for i in range(9) : 
@@ -41,28 +42,70 @@ def conflicts(board)  :
             list.append(board[i])
     return score
 
+def random_zeros(board) : 
+    # Give a random value to the zeroes value by block
+    for i in range(9) : 
+        row = (i // 3) * 3
+        col = (i % 3) * 3
+        block = get_block(board,i)
+        l = []
+        for j in range(3) :
+            for k in range(3) : 
+                if board[row+j][col+k] == 0 : 
+                    value = random.randint(1,9)
+                    while value in block or value in l : 
+                        value = random.randint(1,9)
+                    board[row+j][col+k] = value
+                    l.append(value)
+                     
+    return board
+
+def get_initials(board) : 
+    # Return array containing all the indexes
+    # of the none 0 values
+    l = []
+    for i in range(9) :
+        for j in range(9) :
+            if board[i][j] != 0:
+                l.append(board[i][j])
+    return l
+def swap_block(board,initials) : 
+    # Swap two squares from a random block
+    neighbour = deepcopy(board)
+    block_idx = random.randint(0,8)
+    block = get_block(neighbour,block_idx,ignore=True,initials=initials)  # Now get_block return a list of tuple 
+    to_swap = random.sample(block,2)
+    # print("\nBlock to swap : {}",block_idx)
+    square1 = to_swap[0]
+    square2 = to_swap[1]
+    # print("Square 1 : ",square1,"\n")
+    # print("Square 2 : ",square2,"\n")
+    neighbour[square1[0]][square1[1]],neighbour[square2[0]][square2[1]] = board[square2[0]][square2[1]],board[square1[0]][square1[1]]
+    return neighbour
+
 def simulated_annealing_solver(initial_board):
 
     """Simulated annealing Sudoku solver."""
-
-    current_solution = [row[:] for row in initial_board]
+    board = deepcopy(initial_board)
+    initials = get_initials(initial_board)
+    current_solution = random_zeros(board)
     best_solution = current_solution
     
     current_score = objective_score(current_solution)
     best_score = current_score
 
     temperature = 1.0
-    cooling_rate = ...  #TODO: Adjust this parameter to control the cooling rate
+    cooling_rate = 0.99993 #TODO: Adjust this parameter to control the cooling rate
+    count = 0
+    while temperature > 0.0001 : 
 
-    while temperature > 0.0001:
-
-        try:  
-
+         
+        try : 
             # TODO: Generate a neighbor (Don't forget to skip non-zeros tiles in the initial board ! It will be verified on Inginious.)
             ...
-            neighbor = ...
-           
-
+            neighbor = swap_block(current_solution,initials)
+            
+            
             # Evaluate the neighbor
             neighbor_score = objective_score(neighbor)
 
@@ -82,13 +125,16 @@ def simulated_annealing_solver(initial_board):
                 if (current_score < best_score):
                     best_solution = current_solution
                     best_score = current_score
-
+            # if(count % 10000 == 0) : 
+            #     print_board(current_solution)
+            #     print("\n Best Score : {} Current Score : {} Temp : {}".format(best_score,current_score,temperature))
+            count+=1
             # Cool down the temperature
             temperature *= cooling_rate
-        except:
-
-            print("Break asked")
+        except : 
+            print("Break Asked")
             break
+            
         
     return best_solution, best_score
 
@@ -116,6 +162,14 @@ if __name__ == "__main__":
     # Reading Sudoku from file
     initial_board = read_sudoku_from_file(sys.argv[1])
 
-    print(objective_score(initial_board))
+    # Solving Sudoku using simulated annealing
+    start_timer = time.perf_counter()
+
+    solved_board, current_score = simulated_annealing_solver(initial_board)
+
+    end_timer = time.perf_counter()
+
+    print_board(solved_board)
+    print("\nValue(C):", current_score)
 
     # print("\nTime taken:", end_timer - start_timer, "seconds")
